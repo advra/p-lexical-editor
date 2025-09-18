0ce97f74044398b28df1748cbf40ab682659d324
alonzoa <alonzoa@sb1.sil.net>
2025-09-18 09:46:06 -0700

    Removed pucks procedure until implemented

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,10 +1,8 @@
 import { usersRouter } from '@/modules/user/server/procedure';
 import { createTRPCRouter } from '../init';
-// import { procsRouter } from '@/modules/procs/server/procedure';
 
 export const appRouter = createTRPCRouter({
   users: usersRouter,
-  // procs: procsRouter,
 });
 
 // export type definition of API
