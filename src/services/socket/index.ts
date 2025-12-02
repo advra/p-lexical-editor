7c9a58aaa5b613368050b6b14ed34f6f53925e86
alonzoa <alonzoa@sb1.sil.net>
2025-12-02 15:54:55 -0800

    Removed socket

--- src/services/socket/index.ts
+++
@@ -1,4 +0,0 @@
-// Export all socket services and hooks
-export { sessionSocketService } from './session-socket';
-export { useSessionSocket } from './hooks/use-session-socket';
-export type { SessionStatusData, SessionStatusChangeData } from './types';
